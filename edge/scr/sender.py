84ee4a58d0b4d632e33e6c9a5bb2f87b7c500246
Jorge Alberto Hernandez Figueroa <ing.jahf@outlook.com>
2025-09-21 22:07:31 -0600

    Fix duplicate import in acquire script

--- edge/scr/sender.py
+++ edge/scr/sender.py
@@ -1,5 +1,5 @@
 """Compatibilidad hacia atrás para importaciones previas de InfluxSender."""
 
-from sinks.influx import InfluxSender, to_line
+from .sinks.influx import InfluxSender, to_line
 
 __all__ = ["InfluxSender", "to_line"]
